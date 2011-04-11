e6e06e72d15d0c9d93b9d911e974f27d318c57dd
Michael J. Mitchell <michael@mitchtech.net>
2011-04-11 12:50:29 -0400

    imported ContextProvider

---
+++ src/edu/fsu/cs/contextprovider/ContextBrowserActivity.java
@@ -0,0 +1,207 @@
+/* Copyright (c) 2008-2011 -- CommonsWare, LLC
+
+	 Licensed under the Apache License, Version 2.0 (the "License");
+	 you may not use this file except in compliance with the License.
+	 You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+	 Unless required by applicable law or agreed to in writing, software
+	 distributed under the License is distributed on an "AS IS" BASIS,
+	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 See the License for the specific language governing permissions and
+	 limitations under the License.
+ */
+
+package edu.fsu.cs.contextprovider;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
+import edu.fsu.cs.contextprovider.R;
+
+public class ContextBrowserActivity extends ListActivity {
+	private static final int ADD_ID = Menu.FIRST + 1;
+	private static final int EDIT_ID = Menu.FIRST + 2;
+	private static final int DELETE_ID = Menu.FIRST + 3;
+	private static final String[] PROJECTION = new String[] {
+			ContextProvider.Cntxt._ID, ContextProvider.Cntxt.TITLE,
+			ContextProvider.Cntxt.VALUE };
+	private Cursor contextCursor;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		contextCursor = managedQuery(ContextProvider.Cntxt.CONTENT_URI,
+				PROJECTION, null, null, null);
+
+		ListAdapter adapter = new SimpleCursorAdapter(this, R.layout.row,
+				contextCursor, new String[] { ContextProvider.Cntxt.TITLE,
+						ContextProvider.Cntxt.VALUE }, new int[] {
+						R.id.title, R.id.value });
+
+		setListAdapter(adapter);
+		registerForContextMenu(getListView());
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		contextCursor.close();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE, ADD_ID, Menu.NONE, "Add").setIcon(R.drawable.add)
+				.setAlphabeticShortcut('a');
+
+		return (super.onCreateOptionsMenu(menu));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case ADD_ID:
+			add();
+			return (true);
+		}
+
+		return (super.onOptionsItemSelected(item));
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenu.ContextMenuInfo menuInfo) {
+		menu.add(Menu.NONE, DELETE_ID, Menu.NONE, "Delete")
+				.setIcon(R.drawable.delete).setAlphabeticShortcut('d');
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case DELETE_ID:
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+					.getMenuInfo();
+
+			delete(info.id);
+			return (true);
+		}
+
+		return (super.onOptionsItemSelected(item));
+	}
+
+	private void add() {
+		LayoutInflater inflater = LayoutInflater.from(this);
+		View addView = inflater.inflate(R.layout.add_edit, null);
+		final DialogWrapper wrapper = new DialogWrapper(addView);
+
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.add_context)
+				.setView(addView)
+				.setPositiveButton(R.string.ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								processAdd(wrapper);
+							}
+						})
+				.setNegativeButton(R.string.cancel,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// ignore, just dismiss
+							}
+						}).show();
+	}
+
+	private void delete(final long rowId) {
+		if (rowId > 0) {
+			new AlertDialog.Builder(this)
+					.setTitle(R.string.delete_context)
+					.setPositiveButton(R.string.ok,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int whichButton) {
+									processDelete(rowId);
+								}
+							})
+					.setNegativeButton(R.string.cancel,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int whichButton) {
+									// ignore, just dismiss
+								}
+							}).show();
+		}
+	}
+
+	private void processAdd(DialogWrapper wrapper) {
+		ContentValues values = new ContentValues(2);
+
+		values.put(ContextProvider.Cntxt.TITLE, wrapper.getTitle());
+		values.put(ContextProvider.Cntxt.VALUE, wrapper.getValue());
+
+		getContentResolver()
+				.insert(ContextProvider.Cntxt.CONTENT_URI, values);
+		contextCursor.requery();
+	}
+
+	private void processDelete(long rowId) {
+		Uri uri = ContentUris.withAppendedId(
+				ContextProvider.Cntxt.CONTENT_URI, rowId);
+		getContentResolver().delete(uri, null, null);
+		contextCursor.requery();
+	}
+
+	class DialogWrapper {
+		EditText titleField = null;
+		EditText valueField = null;
+		View base = null;
+
+		DialogWrapper(View base) {
+			this.base = base;
+			valueField = (EditText) base.findViewById(R.id.value);
+		}
+
+		String getTitle() {
+			return (getTitleField().getText().toString());
+		}
+
+		float getValue() {
+			return (new Float(getValueField().getText().toString())
+					.floatValue());
+		}
+
+		private EditText getTitleField() {
+			if (titleField == null) {
+				titleField = (EditText) base.findViewById(R.id.title);
+			}
+
+			return (titleField);
+		}
+
+		private EditText getValueField() {
+			if (valueField == null) {
+				valueField = (EditText) base.findViewById(R.id.value);
+			}
+
+			return (valueField);
+		}
+	}
+}
---
+++ src/edu/fsu/cs/contextprovider/ContextProvider.java
@@ -0,0 +1,326 @@
+/* Copyright (c) 2008-2011 -- CommonsWare, LLC
+
+	 Licensed under the Apache License, Version 2.0 (the "License");
+	 you may not use this file except in compliance with the License.
+	 You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+	 Unless required by applicable law or agreed to in writing, software
+	 distributed under the License is distributed on an "AS IS" BASIS,
+	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 See the License for the specific language governing permissions and
+	 limitations under the License.
+ */
+
+package edu.fsu.cs.contextprovider;
+
+import android.content.*;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.text.TextUtils;
+import java.util.HashMap;
+
+public class ContextProvider extends ContentProvider {
+	private static final String DATABASE_NAME = "cntxt.db";
+	private static final int CNTXT = 1;
+	private static final int CNTXT_ID = 2;
+	private static final UriMatcher MATCHER;
+	private static HashMap<String, String> CNTXT_LIST_PROJECTION;
+
+	public static final class Cntxt implements BaseColumns {
+		public static final Uri CONTENT_URI = Uri
+				.parse("content://edu.fsu.cs.contextprovider/cntxt");
+		public static final String DEFAULT_SORT_ORDER = "title";
+		public static final String TITLE = "title";
+		public static final String VALUE = "value";
+	}
+
+	static {
+		MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+		MATCHER.addURI("edu.fsu.cs.contextprovider", "cntxt", CNTXT);
+		MATCHER.addURI("edu.fsu.cs.contextprovider", "cntxt/#", CNTXT_ID);
+
+		CNTXT_LIST_PROJECTION = new HashMap<String, String>();
+
+		CNTXT_LIST_PROJECTION.put(ContextProvider.Cntxt._ID,
+				ContextProvider.Cntxt._ID);
+		CNTXT_LIST_PROJECTION.put(ContextProvider.Cntxt.TITLE,
+				ContextProvider.Cntxt.TITLE);
+		CNTXT_LIST_PROJECTION.put(ContextProvider.Cntxt.VALUE,
+				ContextProvider.Cntxt.VALUE);
+	}
+
+	public String getDbName() {
+		return (DATABASE_NAME);
+	}
+
+	public int getDbVersion() {
+		return (1);
+	}
+
+	private class DatabaseHelper extends SQLiteOpenHelper {
+		public DatabaseHelper(android.content.Context context) {
+			super(context, DATABASE_NAME, null, 1);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			Cursor c = db
+					.rawQuery(
+							"SELECT name FROM sqlite_master WHERE type='table' AND name='cntxt'",
+							null);
+
+			try {
+				if (c.getCount() == 0) {
+					db.execSQL("CREATE TABLE cntxt (_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, value REAL);");
+
+					ContentValues cv = new ContentValues();
+
+					cv.put(Cntxt.TITLE, "Gravity, Death Star I");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_DEATH_STAR_I);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Earth");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_EARTH);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Jupiter");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_JUPITER);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Mars");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_MARS);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Mercury");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_MERCURY);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Moon");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_MOON);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Neptune");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_NEPTUNE);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Pluto");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_PLUTO);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Saturn");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_SATURN);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Sun");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_SUN);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, The Island");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_THE_ISLAND);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Uranus");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_URANUS);
+					db.insert("cntxt", getNullColumnHack(), cv);
+
+					cv.put(Cntxt.TITLE, "Gravity, Venus");
+					cv.put(Cntxt.VALUE, SensorManager.GRAVITY_VENUS);
+					db.insert("cntxt", getNullColumnHack(), cv);
+				}
+			} finally {
+				c.close();
+			}
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			android.util.Log.w("Cntxt",
+					"Upgrading database, which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS constants");
+			onCreate(db);
+		}
+	}
+
+	private SQLiteDatabase db;
+
+	@Override
+	public boolean onCreate() {
+		db = (new DatabaseHelper(getContext())).getWritableDatabase();
+
+		return (db == null) ? false : true;
+	}
+
+	@Override
+	public Cursor query(Uri url, String[] projection, String selection,
+			String[] selectionArgs, String sort) {
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+		qb.setTables(getTableName());
+
+		if (isCollectionUri(url)) {
+			qb.setProjectionMap(getDefaultProjection());
+		} else {
+			qb.appendWhere(getIdColumnName() + "="
+					+ url.getPathSegments().get(1));
+		}
+
+		String orderBy;
+
+		if (TextUtils.isEmpty(sort)) {
+			orderBy = getDefaultSortOrder();
+		} else {
+			orderBy = sort;
+		}
+
+		Cursor c = qb.query(db, projection, selection, selectionArgs, null,
+				null, orderBy);
+		c.setNotificationUri(getContext().getContentResolver(), url);
+		return c;
+	}
+
+	@Override
+	public String getType(Uri url) {
+		if (isCollectionUri(url)) {
+			return (getCollectionType());
+		}
+
+		return (getSingleType());
+	}
+
+	@Override
+	public Uri insert(Uri url, ContentValues initialValues) {
+		long rowID;
+		ContentValues values;
+
+		if (initialValues != null) {
+			values = new ContentValues(initialValues);
+		} else {
+			values = new ContentValues();
+		}
+
+		if (!isCollectionUri(url)) {
+			throw new IllegalArgumentException("Unknown URL " + url);
+		}
+
+		for (String colName : getRequiredColumns()) {
+			if (values.containsKey(colName) == false) {
+				throw new IllegalArgumentException("Missing column: " + colName);
+			}
+		}
+
+		populateDefaultValues(values);
+
+		rowID = db.insert(getTableName(), getNullColumnHack(), values);
+		if (rowID > 0) {
+			Uri uri = ContentUris.withAppendedId(getContentUri(), rowID);
+			getContext().getContentResolver().notifyChange(uri, null);
+			return uri;
+		}
+
+		throw new SQLException("Failed to insert row into " + url);
+	}
+
+	@Override
+	public int delete(Uri url, String where, String[] whereArgs) {
+		int count;
+		long rowId = 0;
+
+		if (isCollectionUri(url)) {
+			count = db.delete(getTableName(), where, whereArgs);
+		} else {
+			String segment = url.getPathSegments().get(1);
+			rowId = Long.parseLong(segment);
+			count = db.delete(
+					getTableName(),
+					getIdColumnName()
+							+ "="
+							+ segment
+							+ (!TextUtils.isEmpty(where) ? " AND (" + where
+									+ ')' : ""), whereArgs);
+		}
+
+		getContext().getContentResolver().notifyChange(url, null);
+		return count;
+	}
+
+	@Override
+	public int update(Uri url, ContentValues values, String where,
+			String[] whereArgs) {
+		int count;
+
+		if (isCollectionUri(url)) {
+			count = db.update(getTableName(), values, where, whereArgs);
+		} else {
+			String segment = url.getPathSegments().get(1);
+			count = db.update(
+					getTableName(),
+					values,
+					getIdColumnName()
+							+ "="
+							+ segment
+							+ (!TextUtils.isEmpty(where) ? " AND (" + where
+									+ ')' : ""), whereArgs);
+		}
+
+		getContext().getContentResolver().notifyChange(url, null);
+		return count;
+	}
+
+	private boolean isCollectionUri(Uri url) {
+		return (MATCHER.match(url) == CNTXT);
+	}
+
+	private HashMap<String, String> getDefaultProjection() {
+		return (CNTXT_LIST_PROJECTION);
+	}
+
+	private String getTableName() {
+		return ("cntxt");
+	}
+
+	private String getIdColumnName() {
+		return ("_id");
+	}
+
+	private String getDefaultSortOrder() {
+		return ("title");
+	}
+
+	private String getCollectionType() {
+		return ("vnd.android.cursor.dir/vnd.contextprovider.cntxt");
+	}
+
+	private String getSingleType() {
+		return ("vnd.android.cursor.item/vnd.contextprovider.cntxt");
+	}
+
+	private String[] getRequiredColumns() {
+		return (new String[] { "title" });
+	}
+
+	private void populateDefaultValues(ContentValues values) {
+		Long now = Long.valueOf(System.currentTimeMillis());
+		Resources r = Resources.getSystem();
+
+		if (values.containsKey(ContextProvider.Cntxt.VALUE) == false) {
+			values.put(ContextProvider.Cntxt.VALUE, 0.0f);
+		}
+	}
+
+	private String getNullColumnHack() {
+		return ("title");
+	}
+
+	private Uri getContentUri() {
+		return (ContextProvider.Cntxt.CONTENT_URI);
+	}
+}
