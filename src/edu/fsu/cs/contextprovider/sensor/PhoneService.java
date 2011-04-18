7b0998a19b43b1f79ca0d01e19b1b5d4035e4cc3
Michael J. Mitchell <michael@mitchtech.net>
2011-04-18 16:58:59 -0400

    stash and merged ContextBrowser

--- src/edu/fsu/cs/contextprovider/sensor/PhoneService.java
+++ src/edu/fsu/cs/contextprovider/sensor/PhoneService.java
@@ -10,7 +10,11 @@ public class PhoneService extends AbstractContextService {
 
 	private static final String TAG = "PhoneState Service";
 	private static final boolean DEBUG = true;
-
+	public static String PHONE_STATE = "NA";
+	// public static String PHONE_STATE_UPDATE = "NA";
+	public static long PHONE_STATE_UPDATE = 0;	
+	public static long PHONE_STATE_SINCE_UPDATE = 0;	
+	
 	TelephonyManager tm;
 	private PhoneStateListener phoneStateListener = new PhoneStateListener() {
 
@@ -19,30 +23,49 @@ public class PhoneService extends AbstractContextService {
 			super.onCallStateChanged(state, incomingNumber);
 			switch (state) {
 			case TelephonyManager.CALL_STATE_IDLE:
+				PHONE_STATE = "CALL_STATE_IDLE";
+				PHONE_STATE_UPDATE = Long.valueOf(System.currentTimeMillis());
+				if (DEBUG) Log.d(TAG, "State: " + PHONE_STATE);
+				break;				
+			
 			case TelephonyManager.CALL_STATE_RINGING:
+				PHONE_STATE = "CALL_STATE_RINGING";
+				PHONE_STATE_UPDATE = Long.valueOf(System.currentTimeMillis());
+				if (DEBUG) Log.d(TAG, "State: " + PHONE_STATE);
+				break;			
+				
 			case TelephonyManager.CALL_STATE_OFFHOOK:
-				if (DEBUG)
-					Log.d(TAG, "send: " + state);
-				// Amarino.sendDataFromPlugin(PhoneService.this, pluginId,
-				// state);
+				PHONE_STATE = "CALL_STATE_IDLE";
+				PHONE_STATE_UPDATE = Long.valueOf(System.currentTimeMillis());
+				if (DEBUG) Log.d(TAG, "State: " + PHONE_STATE);
 				break;
 			}
 		}
 	};
 
 	@Override
-	public void init() {
+	public void onCreate() {
 		if (!serviceEnabled) {
-
 			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 			// pluginId = prefs.getInt(PhoneEditActivity.KEY_PLUGIN_ID, -1);
 			serviceEnabled = true;
-
 			tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 			tm.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
 		}
 	}
 
+	@Override
+	public void init() {
+		if (!serviceEnabled) {
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			// pluginId = prefs.getInt(PhoneEditActivity.KEY_PLUGIN_ID, -1);
+			serviceEnabled = true;
+			tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+			tm.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+		}
+	}
+	
+	
 	@Override
 	public void onDestroy() {
 		if (serviceEnabled) {
--- src/edu/fsu/cs/contextprovider/sensor/SmsService.java
+++ src/edu/fsu/cs/contextprovider/sensor/SmsService.java
@@ -14,7 +14,12 @@ public class SmsService extends AbstractContextService {
 
 	private static final String TAG = "SMS Service";
 	private static final boolean DEBUG = true;
-
+	public static String SMS_STATE = "NA";
+	public static String SMS_LAST_SENDER = "NA";
+	public static String SMS_LAST_MESSAGE = "NA";
+	public static long SMS_STATE_UPDATE = 0;	
+	public static long SMS_STATE_SINCE_UPDATE = 0;	
+	
 	private static final int MAX_LENGTH = 30;
 	private static final String ACTION_SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 
@@ -35,39 +40,54 @@ public class SmsService extends AbstractContextService {
 
 					for (SmsMessage message : messages) {
 						String msg = message.getMessageBody();
-						// String msgSender =
-						// message.getDisplayOriginatingAddress(); // not need
-						// for sender in this example
+						String msgSender = message.getDisplayOriginatingAddress(); 
 
 						if (msg.length() > MAX_LENGTH)
 							msg = msg.substring(0, MAX_LENGTH);
 
 						if (DEBUG)
 							Log.d(TAG, "send: " + msg);
-						// Amarino.sendDataFromPlugin(context, pluginId, msg);
+						
+						SMS_STATE = "SMS_STATE_IDLE";
+						SMS_STATE_UPDATE = Long.valueOf(System.currentTimeMillis());
+						SMS_LAST_SENDER = msgSender;
+						SMS_LAST_MESSAGE = msg;
+						if (DEBUG) Log.d(TAG, "State: " + SMS_STATE);
+						break;			
+
+
 					}
 				}
 			}
-
 		}
 	};
 
 	@Override
 	public void init() {
 		if (!serviceEnabled) {
-
 			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 //			pluginId = prefs.getInt(SmsEditActivity.KEY_PLUGIN_ID, -1);
-
 			IntentFilter filter = new IntentFilter(ACTION_SMS_RECEIVED);
 			registerReceiver(receiver, filter);
-
 			// don't forget to set plug-in enabled if everything was initialized
-			// fine
 			serviceEnabled = true;
 		}
 	}
 
+	@Override
+	public void onCreate() {
+		if (!serviceEnabled) {
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//			pluginId = prefs.getInt(SmsEditActivity.KEY_PLUGIN_ID, -1);
+			IntentFilter filter = new IntentFilter(ACTION_SMS_RECEIVED);
+			registerReceiver(receiver, filter);
+			// don't forget to set plug-in enabled if everything was initialized
+			serviceEnabled = true;
+		}
+	}
+	
+	
+	
 	@Override
 	public void onDestroy() {
 		if (serviceEnabled) {
