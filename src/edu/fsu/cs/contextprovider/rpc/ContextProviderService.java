a3c759277046e2d4b8e319cc2674a93639bd7470
Chris Meyers <chris.meyers.fsu@gmail.com>
2011-04-19 14:05:13 -0400

    Added rpc functions for proximity

--- src/edu/fsu/cs/contextprovider/rpc/ContextProviderService.java
+++ src/edu/fsu/cs/contextprovider/rpc/ContextProviderService.java
@@ -3,6 +3,7 @@ package edu.fsu.cs.contextprovider.rpc;
 import java.util.HashMap;
 
 import edu.fsu.cs.contextprovider.ContextProvider;
+import edu.fsu.cs.contextprovider.monitor.LocationMonitor;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -18,9 +19,29 @@ public class ContextProviderService extends Service {
 			HashMap<String, String> cntx = ContextProvider.getAllUnordered();
 			return cntx;
 		}
+		
+		public double proximityToAddress(String loc) throws RemoteException {
+			return LocationMonitor.proximityTo(loc);
+		}
+		
+		public double proximityToExact(double longitude, double latitude) throws RemoteException {
+			return LocationMonitor.proximityTo(longitude, latitude);
+		}
+		
+		/**
+		 * Returns array, longitude, latitude
+		 */
+		public double[] getCurrentCoordinates() {
+			double[] values = new double[2];
+			values[0] = LocationMonitor.getLongitude();
+			values[1] = LocationMonitor.getLatitude();
+			return values;
+		}
+		
+		public String getCurrentAddress() {
+			return LocationMonitor.getAddress();
+		}
 	};
-
-
 
 	public int onStartCommand(Intent intent, int flags, int startId){
 		Log.i(TAG, "onStartCommand");
--- src/edu/fsu/cs/contextprovider/rpc/IContextProviderService.aidl
+++ src/edu/fsu/cs/contextprovider/rpc/IContextProviderService.aidl
@@ -2,4 +2,8 @@ package edu.fsu.cs.contextprovider.rpc;
 
 interface IContextProviderService {
 	Map getAll();
+	double proximityToAddress(String loc);
+	double proximityToExact(double longitude, double latitude);
+	double[] getCurrentCoordinates();
+	String getCurrentAddress();
 }
